b3afcc6e86b8edfa018beb37eae027cb096c0bce
DaiKaannn <matteo.cuney@outlook.com>
2022-11-15 10:03:49 +0100

    amélioration

--- client.py
+++ client.py
@@ -1,19 +1,14 @@
 import socket
-hope =True
 
 client_socket = socket.socket()
-# client_socket.connect(("localhost", 10000))
+client_socket.connect(("localhost", 10000))
+message=""
+data=""
 
-while hope == True :
-    client_socket.connect(("localhost", 10000))
+while message != "bye" and data !="bye" and message !="arret" and data !="arret" :
     message=input("Moi : ")
     client_socket.send(message.encode())
     data = client_socket.recv(1024).decode()
     print(f"Message du serveur : {data}")
-
-    if message == "bye" :
-        client_socket.close()
-        hope = False
-
 
 client_socket.close()
--- serveur-asynchro.py
+++ serveur-asynchro.py
@@ -2,17 +2,18 @@ import socket
 
 server_socket = socket.socket()
 server_socket.bind(("localhost", 10000))
+reply=""
+data=""
+server_socket.listen(1)
 
-while True :
-    server_socket.listen(1)
-    reply=input("Moi : ")
+while reply !="arret" and data !="arret":
     conn, address = server_socket.accept()
-    data = conn.recv(1024).decode()
-    conn.send(reply.encode())
-    print(f"Message du client : {data}")
-
-    if data == "bye" :
-        conn, address = server_socket.accept()
+    reply=""
+    data=""
+    while reply != "bye" and data != "bye" and reply != "arret" and data != "arret":
+        reply=input("Moi : ")
         data = conn.recv(1024).decode()
-
-conn.close()
+        conn.send(reply.encode())
+        print(f"Message du client : {data}")
+    conn.close()
+server_socket.close()
