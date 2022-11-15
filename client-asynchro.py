9497a150bff6bb36fe77dc29ed3314d67bb4bd3e
DaiKaannn <matteo.cuney@outlook.com>
2022-11-15 11:59:36 +0100

    amélioration

--- client-asynchro.py
+++ client-asynchro.py
@@ -1,14 +1,19 @@
 import socket
+import threading
 
 client_socket = socket.socket()
 client_socket.connect(("localhost", 10000))
-message=""
-data=""
 
-while message != "bye" and data !="bye" and message !="arret" and data !="arret" :
-    message=input("Moi : ")
-    client_socket.send(message.encode())
-    data = client_socket.recv(1024).decode()
-    print(f"Message du serveur : {data}")
+def reception (client_socket):
+    message=""
+    data=""
+    while message != "bye" and data !="bye" and message !="arret" and data !="arret" :
+        message=input("Moi : ")
+        t1 = threading.Thread(target=reception, args=[client_socket])
+        t1.start()
+        t1.join()
+        client_socket.send(message.encode())
+        data = client_socket.recv(1024).decode()
+        print(f"Message du serveur : {data}")
 
 client_socket.close()
--- serveur-asynchro.py
+++ serveur-asynchro.py
@@ -1,19 +1,25 @@
 import socket
+import threading
 
 server_socket = socket.socket()
 server_socket.bind(("localhost", 10000))
-reply=""
-data=""
 server_socket.listen(1)
 
-while reply !="arret" and data !="arret":
-    conn, address = server_socket.accept()
+def reception (server_socket):
     reply=""
     data=""
-    while reply != "bye" and data != "bye" and reply != "arret" and data != "arret":
-        reply=input("Moi : ")
-        data = conn.recv(1024).decode()
-        conn.send(reply.encode())
-        print(f"Message du client : {data}")
-    conn.close()
+    while reply !="arret" and data !="arret":
+        conn, address = server_socket.accept()
+        reply=""
+        data=""
+        while reply != "bye" and data != "bye" and reply != "arret" and data != "arret":
+            reply=input("Moi : ")
+            data = conn.recv(1024).decode()
+            conn.send(reply.encode())
+            print(f"Message du client : {data}")
+        conn.close()
+
+t1 = threading.Thread(target=reception, args=[server_socket])
+t1.start()
+t1.join()
 server_socket.close()
